0cfac0d0c040ecce163fd2ed3fb0e818135893d3
JoeDuong-dev <chau.duong1995@gmail.com>
2024-02-06 23:40:20 -0800

    Added entry log object

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,12 +1,15 @@
 //Starting value of 0
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
+//Gets input from input field
 function getUserNumberInput() {
     //Using parseInt to convert String input into Integer
     return parseInt(userInput.value);
-}
+} 
 
+//Generates and writes calculation log
 function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
     const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
@@ -15,28 +18,36 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber){
 function add(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult + enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber);   
+    currentResult += enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+    const logEntry = {
+        operation: 'ADD',
+        prevResult: initialResult,
+        number: enteredNumber,
+        result: currentResult
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries[0]);
 }
 
 function subtract(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
+    currentResult -= enteredNumber;
     createAndWriteOutput('-', initialResult, enteredNumber);  
 }
 
 function multiply(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
+    currentResult *= enteredNumber;
     createAndWriteOutput('*', initialResult, enteredNumber);  
 }
 
 function divide(){
     const enteredNumber = getUserNumberInput();
     const initialResult = currentResult;
-    currentResult = currentResult/enteredNumber;
+    currentResult /= enteredNumber;
     createAndWriteOutput('/', initialResult, enteredNumber);  
 }
 //Execute the add function on click
